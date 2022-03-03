f1476224a17245551079e1e2cc7ccb62b682998f
Juan <jgarcia1545@gmail.com>
2022-03-02 16:05:42 -0800

    Started creating some JavaScript as well as began styling the Quiz

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var formEl = document.querySelector("#task-form");
+
+
+// Function to Start Quiz
+var startQuiz = function (event) {
+
+};
+
+// Create a new task
+formEl.addEventListener("submit", startQuiz);

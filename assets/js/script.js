feca3d426ac82e4bf4695335db1e2ed5e7d05be8
Juan <jgarcia1545@gmail.com>
2022-03-02 16:25:37 -0800

    Added Functions and More Styling to CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
-var formEl = document.querySelector("#task-form");
+var beginQuiz = document.querySelector("#start-quiz");
 
 
 // Function to Start Quiz
 var startQuiz = function (event) {
+    console.log("Quiz has began");
+};
+
+// Saves the high scores in local storage
+var saveScores = function () {
+
+};
+
+var loadScores = function () {
 
 };
 
 // Create a new task
-formEl.addEventListener("submit", startQuiz);
+beginQuiz.addEventListener("submit", startQuiz);

e5698f73a8bf395e832c8f508591c6213120e1fd
Juan <jgarcia1545@gmail.com>
2022-03-04 21:12:51 -0800

    Added more style and organized the script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,47 @@
 //Variable Definitions
-var beginQuiz = document.querySelector("#display-questions");
-var newQuestion = document.querySelector("#question-zone");
-var timer = document.querySelector("#timer-start");
-var multipleChoice = document.querySelector("#multiple-choice");
+var infoQuiz = document.querySelector("#quiz-info");
+var beginQuiz = document.querySelector("#start-quiz");
+var timer = document.querySelector("#timer-start"); //Setting the timer DOM
+var displayQuestion = document.querySelector("#question");
+var multipleChoice = document.querySelector("#multipl-choice");
+var choice1 = document.querySelector("#choice1");
+var choice2 = document.querySelector("#choice2");
+var choice3 = document.querySelector("#choice3");
+var choice4 = document.querySelector("#choice4");
+var score = 0; // User Score
+var timeLeft = 59; // Start Timer (Begins at 59 because of 1 sec delay in countDonw())
 
 // Creating the questions with answers and storing them in an array as an object
 var quizQuestions = [
     {
-        //1. <> 2. ~ 3. ==! 4. !==
         question: "Which operator returns true if the two compared values are not equal?",
+        choices: ["<>", "~", "==!", "!=="],
         answer: "!=="
     },
     {
-        // 1. self 2. object 3. target 4. source
         question: "Which property references the DOM object that dispatched an event?",
+        choices: ["self", "object", "target", "source"],
         answer: "target"
     },
     {
-        // 1. JSON.fromString(); 2. JSON.parse() 3. JSON.toObject() 4. JSON.stringify()
         question: "Which method converts JSON data to a JavaScript object?",
+        choices: ["JSON.fromString();", "JSON.parse()", "JSON.toObject()", "JSON.stringify()"],
         answer: "JSON.parse()"
     },
     {
-        // 1. Object.get() 2. Object.loop() 3. Object.each() 4. Object.keys()
         question: "Which Object method returns an iterable that can be used to iterate over the properties of an object?",
+        choices: ["Object.get()", "Object.loop()", "Object.each()", "Object.keys()"],
         answer: "Object.keys()"
     },
     {
-        // 1. this 2. catch 3. function 4. array
         question: "Which of the following is not a keyword in JavaScript?",
+        choices: ["this", "catch", "function", "array"],
         answer: "array"
     }
 ]
 
 // Starts the timer
 function countDown() {
-    var timeLeft = 59;
 
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
@@ -45,14 +51,12 @@ function countDown() {
             timer.textContent = 'Time: ' + timeLeft;
             // Decrement `timeLeft` by 1
             timeLeft--;
-        } else if (timeLeft === 1) {
-            timer.textContent = 'Time: ' + timeLeft;
-            timeLeft--;
         } else {
             // Once `timeLeft` gets to 0, set `timer` to an empty string
-            timer.textContent = '';
+            timer.textContent = 'Time: ' + '0';
             // Use `clearInterval()` to stop the timer
             clearInterval(timeInterval);
+            endQuiz();
         }
     }, 1000);
 }
@@ -62,11 +66,31 @@ function countDown() {
 var startQuiz = function (event) {
     event.preventDefault();
     countDown();
+    infoQuiz.setAttribute("style", "display: none;");
 
-    for(i=0;i<quizQuestions.length; i++) {
-        newQuestion.textContent = quizQuestions[i].question;
-        
+    for (i = 0; i < quizQuestions.length; i++) {
+        displayQuestion.textContent = quizQuestions[i].question;
+        choice1.textContent = quizQuestions[i].choices[0];
+        choice2.textContent = quizQuestions[i].choices[1];
+        choice3.textContent = quizQuestions[i].choices[2];
+        choice4.textContent = quizQuestions[i].choices[3];
+        //console.log(choice1.textContent);
+
+        if(choice1.textContent === quizQuestions.answer) {
+            console.log("no");
+        } else if(choice2.textContent === quizQuestions.answer) {
+            console.log("no");
+        } else if(choice3.textContent === quizQuestions.answer) {
+            console.log("no");
+        } else {
+            console.log("didnt check");
+        }
     }
+
+};
+
+var endQuiz = function () {
+    // multipleChoice.setAttribute("style", "display: none;");
 
 };
 
